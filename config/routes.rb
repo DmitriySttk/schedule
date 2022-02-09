612a6bc3627c786c259e5d2d4ebe84db2ce75205
faceroll <set.frnx@gmail.com>
2022-02-09 17:05:57 +0300

    users controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :activities
+  resources :users
   root 'home#index'
   get 'home/about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
