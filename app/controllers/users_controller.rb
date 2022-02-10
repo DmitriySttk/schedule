d28f4fd4033f16fd2d7caab62f633f11fcf0613b
faceroll <set.frnx@gmail.com>
2022-02-10 14:50:33 +0300

    added activity-user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @run_activities = @user.run_activities
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_194031) do
+ActiveRecord::Schema.define(version: 2022_02_10_075027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,23 @@ ActiveRecord::Schema.define(version: 2022_02_01_194031) do
     t.string "map"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "activity_id"
+    t.integer "parent_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "registrations", force: :cascade do |t|
+    t.bigint "activity_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["activity_id"], name: "index_registrations_on_activity_id"
+    t.index ["user_id"], name: "index_registrations_on_user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -49,4 +66,6 @@ ActiveRecord::Schema.define(version: 2022_02_01_194031) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "registrations", "activities"
+  add_foreign_key "registrations", "users"
 end
