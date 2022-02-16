ce4e3f35bf2b040cb91cae8af5f20d947605b60a
faceroll <set.frnx@gmail.com>
2022-02-16 10:07:25 +0300

    changes for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
 
     root 'home#index'
     get 'home/search'
-  end
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
